df9f9beba8b28853578fbd8aedd55f8099a4c966
xxx0211 <bimo211@gmail.com>
2021-06-01 23:51:54 +0300

    fix add value

--- src/main/java/CuckooHashing.java
+++ src/main/java/CuckooHashing.java
@@ -19,9 +19,25 @@ public class CuckooHashing {
         } else if (table1[index2] == null) {
             table1[index2] = node;
         } else{
-            for (; index2 < table1.length; index2++){
-                if(table1[index2] == null){
-                    table1[index2] = node;
+            Node tmp = table1[index2];
+            table1[index2] = node;
+            int i = hash2(tmp.value);
+            if(table1[i] == null){
+                table1[i] = tmp;
+            }else{
+                node = tmp;
+                tmp = table1[i];
+                table1[i] = node;
+                int i1 = hash2(tmp.value);
+                if(table1[i1] == null){
+                    table1[i1] = tmp;
+                }else{
+                    for (; i1 < table1.length; i1++){
+                        if(table1[i1] == null){
+                            table1[i1] = tmp;
+                            break;
+                        }
+                    }
                 }
             }
         }
